952a01935455f64463b86f8f7cce176f2a0dbb64
Gustav Behm <gustav@upvest.co>
2018-04-16 23:22:25 +0200

    A mapper that seems to work

--- src/main/scala/terminology/Adjectives.scala
+++ src/main/scala/terminology/Adjectives.scala
@@ -7,7 +7,28 @@ package object adjectives {
   trait Adjective[F[_]] {
     def drop[U](f: F[U]): U
     def mk[R](r: R): F[R]
-    def mapU[U, R](fu: F[U], f: U => R): F[R] = mk(f(drop(fu)))
+  }
+
+  trait AdjectiveMapper[F[_], U, R] {
+    type Out[S]
+    def mapU[S](fu: F[R], f: U => S): F[Out[S]]
+  }
+
+  implicit def adjMap[F[_], U](implicit A: Adjective[F]) =
+    new AdjectiveMapper[F, U, U] {
+      type Out[S] = S
+      def mapU[S](fu: F[U], f: U => S): F[Out[S]] = A.mk(f(A.drop(fu)))
+    }
+
+  implicit def adjMap2[F[_], G[_], U, R](implicit A: Adjective[F], AM: AdjectiveMapper[G, U, R]) =
+    new AdjectiveMapper[F, U, G[R]] {
+      type Out[S] = G[AM.Out[S]]
+      def mapU[S](fu: F[G[R]], f: U => S): F[Out[S]] = A.mk(AM.mapU(A.drop(fu), f))
+    }
+
+  implicit class AdjOps[F[_], T](ft: F[T]) {
+    def mapU[U, R](f: U => R)(implicit AM: AdjectiveMapper[F, U, T]): F[AM.Out[R]] =
+      AM.mapU(ft, f)
   }
 
   //implicit def dropAdj[F[_], U](f: F[U])(implicit Adj: Adjective[F, U]): U =
@@ -34,25 +55,25 @@ package object adjectives {
   object implicits extends AdjectiveImplicits0
 
   trait AdjectiveImplicits0 extends AdjectiveImplicits1 {
-    implicit class AdjectiveOps3[G[_], F[_], H[_], U](fghu: F[G[H[U]]])(implicit Adj: Adjective[λ[α => F[G[H[α]]]]]) {
-      def drop: U = Adj.drop(fghu)
-      def mapU[R](f: U => R): F[G[H[R]]] = Adj.mapU(fghu, f)
-    }
+    //implicit class AdjectiveOps3[G[_], F[_], H[_], U](fghu: F[G[H[U]]])(implicit Adj: Adjective[λ[α => F[G[H[α]]]]]) {
+      //def drop: U = Adj.drop(fghu)
+      //def mapU[R](f: U => R): F[G[H[R]]] = Adj.mapU(fghu, f)
+    //}
 
-    implicit def stackedAdj[F[_], G[_]](implicit
-      af: Adjective[F],
-      ag: Adjective[G]
-    ): Adjective[λ[α => F[G[α]]]] = new Adjective[λ[α => F[G[α]]]] {
-      def drop[U](x: F[G[U]]): U = ag.drop(af.drop(x))
-      def mk[R](r: R): F[G[R]] = af.mk(ag.mk(r))
-    }
+    //implicit def stackedAdj[F[_], G[_]](implicit
+      //af: Adjective[F],
+      //ag: Adjective[G]
+    //): Adjective[λ[α => F[G[α]]]] = new Adjective[λ[α => F[G[α]]]] {
+      //def drop[U](x: F[G[U]]): U = ag.drop(af.drop(x))
+      //def mk[R](r: R): F[G[R]] = af.mk(ag.mk(r))
+    //}
   }
 
   trait AdjectiveImplicits1 {
-    implicit class AdjectiveOps2[G[_], F[_], U](fgu: F[G[U]])(implicit Adj: Adjective[λ[α => F[G[α]]]]) {
-      def drop: U = Adj.drop(fgu)
-      def mapU[R](f: U => R): F[G[R]] = Adj.mapU(fgu, f)
-    }
+    //implicit class AdjectiveOps2[G[_], F[_], U](fgu: F[G[U]])(implicit Adj: Adjective[λ[α => F[G[α]]]]) {
+      //def drop: U = Adj.drop(fgu)
+      //def mapU[R](f: U => R): F[G[R]] = Adj.mapU(fgu, f)
+    //}
     //implicit class AdjectiveOps[F[_], U](fu: F[U])(implicit Adj: Adjective[F]) {
       //def drop: U = Adj.drop(fu)
       //def mapU[R](f: U => R): F[R] = Adj.mapU(fu, f)
@@ -101,11 +122,7 @@ package object adjectives {
     implicit val secret = deriveAdjective[Secret]
   }
 
+  Hashed(Weak(Secret("lol"))) mapU { (s: String) => s.length } : Hashed[Weak[Secret[Int]]]
 
-  import implicits._
-  implicitly[Adjective[λ[α => Weak[Secret[α]]]]].mapU(
-    Weak(Secret("lol")),
-    { (s: String) => s.length }) : Weak[Secret[Int]]
-
-  //Hashed(Weak(Secret("lol"))) mapU { (s: String) => s.length }
+  Hashed(Weak(Secret("lol"))) mapU { (s: Secret[String]) => 7 } : Hashed[Weak[Int]]
 }
