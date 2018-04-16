127f4c6451c9fd556dc92c241a6efc283e32f604
Gustav Behm <gustav@upvest.co>
2018-04-16 23:30:24 +0200

    Remove unused code

--- src/main/scala/terminology/Adjectives.scala
+++ src/main/scala/terminology/Adjectives.scala
@@ -31,9 +31,6 @@ package object adjectives {
       AM.mapU(ft, f)
   }
 
-  //implicit def dropAdj[F[_], U](f: F[U])(implicit Adj: Adjective[F, U]): U =
-    //Adj.drop(f)
-
   trait Point[R[_]] {
     def point[A](a: A): R[A]
     def copoint[A](ra: R[A]): A
@@ -51,35 +48,6 @@ package object adjectives {
       def drop[U](x: F[U]): U = gen.fr.copoint(gen.to(x))
       def mk[R](r: R): F[R] = gen.from(gen.fr.point(r))
     }
-
-  object implicits extends AdjectiveImplicits0
-
-  trait AdjectiveImplicits0 extends AdjectiveImplicits1 {
-    //implicit class AdjectiveOps3[G[_], F[_], H[_], U](fghu: F[G[H[U]]])(implicit Adj: Adjective[λ[α => F[G[H[α]]]]]) {
-      //def drop: U = Adj.drop(fghu)
-      //def mapU[R](f: U => R): F[G[H[R]]] = Adj.mapU(fghu, f)
-    //}
-
-    //implicit def stackedAdj[F[_], G[_]](implicit
-      //af: Adjective[F],
-      //ag: Adjective[G]
-    //): Adjective[λ[α => F[G[α]]]] = new Adjective[λ[α => F[G[α]]]] {
-      //def drop[U](x: F[G[U]]): U = ag.drop(af.drop(x))
-      //def mk[R](r: R): F[G[R]] = af.mk(ag.mk(r))
-    //}
-  }
-
-  trait AdjectiveImplicits1 {
-    //implicit class AdjectiveOps2[G[_], F[_], U](fgu: F[G[U]])(implicit Adj: Adjective[λ[α => F[G[α]]]]) {
-      //def drop: U = Adj.drop(fgu)
-      //def mapU[R](f: U => R): F[G[R]] = Adj.mapU(fgu, f)
-    //}
-    //implicit class AdjectiveOps[F[_], U](fu: F[U])(implicit Adj: Adjective[F]) {
-      //def drop: U = Adj.drop(fu)
-      //def mapU[R](f: U => R): F[R] = Adj.mapU(fu, f)
-    //}
-
-  }
 
   case class Weak[T](t: T)
   object Weak {
